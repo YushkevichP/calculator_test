3e3836ec596707b6cfd9e6fef3efdf8ec0fe0185
YushkevichP <7146514@gmail.com>
2024-10-11 22:26:04 +0300

    add appendSymbol fun + onclicklisteners

--- app/src/main/java/com/example/calculator2024/CalculatorFragment.kt
+++ app/src/main/java/com/example/calculator2024/CalculatorFragment.kt
@@ -7,19 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.calculator2024.databinding.FragmentCalculatorBinding
 
-class CalculatorFragment:Fragment() {
+class CalculatorFragment : Fragment() {
 
-    private var _binding : FragmentCalculatorBinding? = null
-    private val binding get() = requireNotNull(_binding){
-        "Binding problem"
-    }
+    private var _binding: FragmentCalculatorBinding? = null
+    private val binding
+        get() = requireNotNull(_binding) {
+            "Binding problem"
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentCalculatorBinding.inflate(inflater,container, false)
+        return FragmentCalculatorBinding.inflate(inflater, container, false)
             .also {
                 _binding = it
             }
@@ -29,9 +30,66 @@ class CalculatorFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initButtons()
+    }
 
+    private fun initButtons() {
+        with(binding) {
+
+            button0.setOnClickListener {
+                appendSymbol("0")
+            }
+
+            button1.setOnClickListener {
+                appendSymbol("1")
+            }
+
+            button2.setOnClickListener {
+                appendSymbol("2")
+            }
+
+            button3.setOnClickListener {
+                appendSymbol("3")
+            }
+
+            button4.setOnClickListener {
+                appendSymbol("4")
+            }
+            button5.setOnClickListener {
+                appendSymbol("5")
+            }
+
+            button6.setOnClickListener {
+                appendSymbol("6")
+            }
+
+            button7.setOnClickListener {
+                appendSymbol("7")
+            }
+
+            button8.setOnClickListener {
+                appendSymbol("8")
+            }
+
+            button9.setOnClickListener {
+                appendSymbol("9")
+            }
+
+            buttonAC.setOnClickListener {
+               inputPlace.text = ""
+            }
+
+            buttonBraces.setOnClickListener {
+                appendSymbol("(")
+            }
+        }
+    }
+
+    private fun appendSymbol(symbol: String) {
+        binding.inputPlace.append(symbol)
 
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
