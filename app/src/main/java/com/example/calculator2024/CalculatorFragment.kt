f5c9fc8a6f855ba0d944da1dd8b9d09d4e11f422
YushkevichP <7146514@gmail.com>
2024-10-12 14:54:57 +0300

    added checking of first symbol is it math

--- app/src/main/java/com/example/calculator2024/CalculatorFragment.kt
+++ app/src/main/java/com/example/calculator2024/CalculatorFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.calculator2024.databinding.FragmentCalculatorBinding
+import kotlin.system.exitProcess
 
 class CalculatorFragment : Fragment() {
 
@@ -31,6 +33,8 @@ class CalculatorFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initButtons()
+
+
     }
 
     private fun initButtons() {
@@ -86,12 +90,40 @@ class CalculatorFragment : Fragment() {
             buttonBackspace.setOnClickListener {
                 deleteSymbol()
             }
+
+            buttonPlus.setOnClickListener {
+                appendSymbol("+")
+            }
+
+
+            buttonMinus.setOnClickListener {
+                appendSymbol("-")
+            }
+
+            buttonX.setOnClickListener {
+                appendSymbol("х")
+            }
+
+
+            buttonDivide.setOnClickListener {
+                appendSymbol("/")
+            }
+
+            buttonEqual.setOnClickListener {
+                // TODO:  equals logic
+            }
+
+            buttonDot.setOnClickListener {
+                appendSymbol(".")
+            }
+
+
         }
     }
 
     private fun deleteSymbol() {
-        val inputText = binding.inputPlace.text
 
+        val inputText = binding.inputPlace.text
         when {
             inputText.length > 1 -> {
                 binding.inputPlace.text = inputText.dropLast(1)
@@ -107,9 +139,15 @@ class CalculatorFragment : Fragment() {
 
     private fun appendSymbol(symbol: String) {
         if (binding.inputPlace.text[0] == '0') {
-            binding.inputPlace.text = ""
-        }
-        binding.inputPlace.append(symbol)
+            //добавлена проверка на первый символ в строке (если это математический, то сообщение)
+            if (symbol.equals("+") || symbol.equals("х") || symbol.equals("/") || symbol.equals("-")) {
+                Toast.makeText(requireContext(), "Please input the number", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                binding.inputPlace.text = ""
+                binding.inputPlace.append(symbol)
+            }
+        } else binding.inputPlace.append(symbol)
     }
 
 
